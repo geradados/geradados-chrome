70a53c7a956921f0cedc2c4f79b74b569352cde8
Christian Fortes <giovanirushl@gmail.com>
2015-11-11 00:49:13 -0200

    Initial Commit

---
+++ js/background.js
@@ -0,0 +1,12 @@
+// Set contador no icone browser action
+// var i = 0;
+// window.setInterval(function () {
+// 	chrome.browserAction.setBadgeText({
+// 		text: String(i)
+// 	});
+
+// 	chrome.browserAction.setBadgeBackgroundColor({
+// 		color: "#000"
+// 	});
+// 	i++;
+// }, 1000);
